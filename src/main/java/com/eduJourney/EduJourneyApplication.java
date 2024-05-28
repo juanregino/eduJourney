5f1b825512cdc5b9453d06599ae1751c346f1b59
juanregino <juanreginopenagos@gmail.com>
2024-05-28 08:15:38 -0500

    first commit

---
+++ src/main/java/com/eduJourney/EduJourneyApplication.java
@@ -0,0 +1,13 @@
+package com.eduJourney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduJourneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduJourneyApplication.class, args);
+	}
+
+}
